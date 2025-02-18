0c8d9a8a8f2939ae27bf82622618feec89874d36
alexsule <alex.njoroge@student.moringaschool.com>
2025-02-18 05:54:02 -0800

    Attempt One

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Nairobi, Kenya</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+254110876545</p>
                 </div>
               </div>
             </div>
